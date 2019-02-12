3ea1b56acc61ce7a61b50f9c5a99fe92eb0b6548
nrasmuss <35875357+nrasmuss@users.noreply.github.com>
2019-02-12 18:23:31 -0500

    Add files via upload
    PLP Assignment 2 Code

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // allows standard input and output operations
+
+int main() {
+
+    std::cout << "Hello World!\n"; //standard character output device insertion operator content semicolon
+    return 0;
+}
